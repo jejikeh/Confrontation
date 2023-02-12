eba0a1bc45c5f760554eb17a9fbf0e2426a84b1e
jejikeh <jejikeh@gmail.com>
2023-02-12 04:17:15 +0300

    istartable interface

--- Assets/Scripts/Wooff.ECS/IStartable.cs
+++ Assets/Scripts/Wooff.ECS/IStartable.cs
@@ -1,7 +1,16 @@
-﻿namespace Wooff.ECS
+﻿using System.Threading.Tasks;
+
+namespace Wooff.ECS
 {
     public interface IStartable
     {
-        
+        public void StartOneThread();
+        public Task StartParallelAsync();
+    }
+    
+    public interface IStartable<in T> : IStartable
+    {
+        public void StartOneThread(T data);
+        public Task StartParallelAsync(T data);
     }
 }
