99537bdc8c0c96558908a3ac3ab7485e4e3e5036
jejikeh <jejikeh@gmail.com>
2023-02-16 22:12:37 +0300

    new systems to update smooth components

--- Assets/Scripts/Core/Systems/SmoothRotateAround.cs
+++ Assets/Scripts/Core/Systems/SmoothRotateAround.cs
@@ -1,7 +1,21 @@
-﻿namespace Core.Systems
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core.Components.SmoothRotateComponent;
+using Wooff.ECS.Contexts;
+using Wooff.MonoIntegration;
+
+namespace Core.Systems
 {
-    public class SmoothRotateAround
+    public class SmoothRotateAround : IMonoSystem
     {
-        
+        public void Process(float timeScale, IContext<IMonoEntity, List<IMonoEntity>> data)
+        {
+            foreach (var smoothRotate in data.Items
+                         .Where(x => x.ContextContains<SmoothRotate>())
+                         .Select(x => x.ContextGet<SmoothRotate>()))
+            {
+                smoothRotate.Update(timeScale);
+            }
+        }
     }
 }
--- Assets/Scripts/Core/Systems/UpdateSmoothTranslate.cs
+++ Assets/Scripts/Core/Systems/UpdateSmoothTranslate.cs
@@ -1,7 +1,22 @@
-﻿namespace Core.Systems
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core.Components.SmoothTranslateComponent;
+using Wooff.ECS.Contexts;
+using Wooff.MonoIntegration;
+
+namespace Core.Systems
 {
-    public class UpdateSmoothTranslate
+    public class UpdateSmoothTranslate : IMonoSystem
     {
-        
+        public void Process(float timeScale, IContext<IMonoEntity, List<IMonoEntity>> data)
+        {
+            foreach (var smoothTranslate in data.Items
+                         .Where(x => x.ContextContains<SmoothTranslate>())
+                         .Select(x => x.ContextGet<SmoothTranslate>()))
+            {
+                smoothTranslate.UpdatePosition(timeScale);
+                smoothTranslate.UpdateVelocity(timeScale);
+            }
+        }
     }
 }
