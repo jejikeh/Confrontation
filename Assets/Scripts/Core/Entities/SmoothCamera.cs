038a44da7de2175f30f9f11c2d1a7cc284968d0a
jejikeh <jejikeh@gmail.com>
2023-02-16 22:12:59 +0300

    two new entities for camera movement

--- Assets/Scripts/Core/Entities/SmoothCamera.cs
+++ Assets/Scripts/Core/Entities/SmoothCamera.cs
@@ -1,16 +1,25 @@
-﻿using Core.Components.SmoothLookAtTargetComponent;
+﻿using System;
+using Core.Components.SmoothLookAtTargetComponent;
+using Core.Components.SmoothRotateComponent;
 using UnityEngine;
 using Wooff.MonoIntegration;
 
 namespace Core.Entities
 {
-    public class Bob : MonoEntity
+    [RequireComponent(typeof(Camera))]
+    public class SmoothCamera : MonoEntity
     {
         [SerializeField] private SmoothLookAtTargetConfig _smoothLookAtTargetConfig;
-
+        private SmoothLookAtTarget _smoothLookAtTarget;
+        
         private void Start()
         {
-            ContextAdd(new SmoothLookAtTarget(_smoothLookAtTargetConfig, this));
+            _smoothLookAtTarget = (SmoothLookAtTarget)ContextAdd(new SmoothLookAtTarget(_smoothLookAtTargetConfig, this));
+        }
+
+        private void Update()
+        {
+            _smoothLookAtTarget.UpdateOffset(Input.mouseScrollDelta.y * 10f);
         }
     }
 }
--- Assets/Scripts/Core/Entities/Target.cs
+++ Assets/Scripts/Core/Entities/Target.cs
@@ -1,16 +1,36 @@
-﻿using Core.Components.SmoothLookAtTargetComponent;
+﻿using System;
+using Core.Components.SmoothLookAtTargetComponent;
+using Core.Components.SmoothRotateComponent;
+using Core.Components.SmoothTranslateComponent;
 using UnityEngine;
 using Wooff.MonoIntegration;
 
 namespace Core.Entities
 {
-    public class Bob : MonoEntity
+    public class Target : MonoEntity
     {
-        [SerializeField] private SmoothLookAtTargetConfig _smoothLookAtTargetConfig;
-
+        [SerializeField] private SmoothRotateConfig _smoothRotateConfig;
+        private SmoothRotate _smoothRotate;
+        
+        [SerializeField] private SmoothTranslateConfig _smoothTranslateConfig;
+        private SmoothTranslate _smoothTranslate;
+        
         private void Start()
         {
-            ContextAdd(new SmoothLookAtTarget(_smoothLookAtTargetConfig, this));
+            _smoothTranslate = (SmoothTranslate)ContextAdd(new SmoothTranslate(_smoothTranslateConfig, this));
+            _smoothRotate = (SmoothRotate)ContextAdd(new SmoothRotate(_smoothRotateConfig, this));
+            ContextAdd(new TargetLootAt(null, this));
+        }
+
+        private void Update()
+        {
+            var input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            _smoothTranslate.SetMovementDirection(input);
+            
+            if (Input.GetKey(KeyCode.Q))
+                _smoothRotate.Rotate(new Vector3(0, -1, 0));
+            else if(Input.GetKey(KeyCode.E))
+                _smoothRotate.Rotate(new Vector3(0,1,0));
         }
     }
 }
