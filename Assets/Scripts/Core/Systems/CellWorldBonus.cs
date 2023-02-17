08037e2d62874243b27e818057b646b0d8bdc59f
jejikeh <jejikeh@gmail.com>
2023-02-17 22:40:21 +0300

    bonus and camera translates to clicked item system

--- Assets/Scripts/Core/Systems/CellWorldBonus.cs
+++ Assets/Scripts/Core/Systems/CellWorldBonus.cs
@@ -1,7 +1,31 @@
-﻿namespace Core.Systems
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core.Components.CellComponent;
+using Core.Entities.MetricsKeeper;
+using UnityEngine;
+using Wooff.ECS.Contexts;
+using Wooff.MonoIntegration;
+
+namespace Core.Systems
 {
-    public class CellWorldBonus
+    public class CellWorldBonus : IMonoSystem
     {
+        private readonly float _time = 10f;
+        private float _timer;
         
+        public void Process(float timeScale, IContext<IMonoEntity, List<IMonoEntity>> data)
+        {
+            if (_timer < _time)
+            {
+                _timer += Time.deltaTime * timeScale;
+                return;
+            }
+
+            var cells = data.Items.Select(x => x.ContextGetAs<Cell>()).Where(x => x is not null);
+            foreach (var cell in cells)
+                MetricsKeeperManager.GetMetric(cell.Config.MetricTypeBonusTo).AddToMetric(cell.GetBonusAmount());
+
+            _timer = 0;
+        }
     }
 }
