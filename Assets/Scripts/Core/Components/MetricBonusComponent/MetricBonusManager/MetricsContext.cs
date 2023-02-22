d67d42e0a20f214127630a954a9242f4460f27bb
jejikeh <jejikeh@gmail.com>
2023-02-22 03:20:18 +0300

    metric bonus handler

---
+++ Assets/Scripts/Core/Components/MetricBonusComponent/MetricBonusManager/MetricsContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Wooff.ECS.Contexts;
+
+namespace Core.Components.MetricBonusComponent.MetricBonusManager
+{
+    public class MetricsContext : Context<IMetricBonus, List<IMetricBonus>>, IListContext<IMetricBonus>
+    {
+        
+    }
+}
