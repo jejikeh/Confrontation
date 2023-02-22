d86dc040fdd6a31f63449833ba64235b5afc95e9
jejikeh <jejikeh@gmail.com>
2023-02-22 03:21:03 +0300

    random component

---
+++ Assets/Scripts/Core/Components/RandomableComponent/RandomableConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using Wooff.ECS;
+
+namespace Core.Components.RandomableComponent
+{
+    [Serializable]
+    public class RandomableConfig : IConfig
+    {
+        [Range(0, 100)]
+        public int Chance;
+    }
+}
