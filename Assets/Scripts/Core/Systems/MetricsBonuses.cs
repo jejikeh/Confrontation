5d8c6fc0ab36855a16b44d5d570b881ae3eb7be4
jejikeh <jejikeh@gmail.com>
2023-02-22 03:24:03 +0300

    update bonus system to work with new bonus ontext

--- Assets/Scripts/Core/Systems/MetricsBonuses.cs
+++ Assets/Scripts/Core/Systems/MetricsBonuses.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Core.Components.CellComponent;
-using Core.Components.MetricBonusComponent;
+using Core.Components.MetricBonusComponent.MetricBonusManager;
 using Core.Entities.MetricsKeeper;
-using UnityEngine;
 using Wooff.ECS.Contexts;
 using Wooff.MonoIntegration;
 
@@ -11,20 +9,21 @@ namespace Core.Systems
 {
     public class MetricsBonuses : IMonoSystem
     {
-        private readonly float _time = 10f;
+        private const float Time = 10f;
         private float _timer;
         
         public void Process(float timeScale, IContext<IMonoEntity, List<IMonoEntity>> data)
         {
-            if (_timer < _time)
+            if (_timer < Time)
             {
-                _timer += Time.deltaTime * timeScale;
+                _timer += UnityEngine.Time.deltaTime * timeScale;
                 return;
             }
 
-            var metricBonus = data.Items.Select(x => x.ContextGet<MetricBonus>()).Where(x => x is not null);
-            foreach (var bonus in metricBonus)
-                MetricsKeeperManager.GetMetric(bonus.Config.MetricType).AddToMetric(bonus.GetBonusAmount());
+            var metricBonus = data.Items.Select(x => x.ContextGet<MetricBonusesHandler>()).Where(x => x is not null);
+            foreach (var bonus in metricBonus.Select(x => x.Items))
+                foreach (var metric in bonus)
+                    MetricsKeeperManager.GetMetric(metric.MetricType).AddToMetric(metric.GetBonusAmount());
 
             _timer = 0;
         }
