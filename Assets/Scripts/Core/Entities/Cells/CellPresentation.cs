dc3ceebd5c1c7c7e01c9d9c6cfe7f1a2fee671fa
jejikeh <jejikeh@gmail.com>
2023-02-22 03:23:32 +0300

    update mono entity to work with updates component

--- Assets/Scripts/Core/Entities/Cells/CellPresentation.cs
+++ Assets/Scripts/Core/Entities/Cells/CellPresentation.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Core.Components.CellComponent;
 using Core.Components.ClickableComponent;
 using Core.Components.ClickComponent;
 using Core.Components.InformationComponent;
+using Core.Components.MetricBonusComponent;
+using Core.Components.MetricBonusComponent.MetricBonusManager;
+using Core.Components.MetricComponent;
+using Core.Components.UIComponents.ScreenComponent;
 using Core.Components.UIComponents.WindowComponent;
 using Core.Components.UIComponents.WindowComponent.Windows;
 using Core.Entities.UI;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using Wooff.MonoIntegration;
 
 namespace Core.Entities.Cells
@@ -16,8 +23,10 @@ namespace Core.Entities.Cells
     [RequireComponent(typeof(MeshCollider))]
     public class CellPresentation : MonoEntity
     {
+        private float _yPosition;
         private void Start()
         {
+            _yPosition = transform.position.y;
             // TODO: unsub
             var clickable = (Clickable)ContextAdd(new Clickable(
                 new ClickableConfig 
@@ -29,9 +38,34 @@ namespace Core.Entities.Cells
             clickable.OnClick += OnClick;
         }
 
-        private void OnClick(object sender, EventArgs e)
+        private async void OnClick(object sender, EventArgs e)
         {
-            (ScreenPlacer.GetWindow(WindowType.Information) as InformationWindow)?.ShowInformation(ContextGet<Information>());
+            transform.DOComplete();
+            await transform.DOPunchScale(Vector3.up,0.1f).AsyncWaitForCompletion();
+            
+            if(ScreenPlacer.GetScreenState() == ScreenState.Information)
+                (ScreenPlacer.GetWindow(WindowType.Information) as InformationWindow)?
+                    .ShowInformation(ContextGet<Information>());
+            else if (ScreenPlacer.GetScreenState() == ScreenState.Build)
+            {
+                ContextGet<MetricBonusesHandler>().ContextAdd(new MetricBonus(new MetricBonusConfig()
+                {
+                    BonusAmount = 10,
+                    MetricType = MetricType.SpeedCreationUnits
+                }));
+            }
+        }
+
+        public async void OnMouseEnter()
+        {
+            transform.DOComplete();
+            await transform.DOMoveY(_yPosition + 0.15f, 0.25f).AsyncWaitForCompletion();
+        }
+
+        public async void OnMouseExit()
+        {
+            transform.DOComplete();
+            await transform.DOMoveY(_yPosition, 0.25f).AsyncWaitForCompletion();
         }
     }
 }
---
+++ Assets/Scripts/Core/Entities/Cells/CellWorldCreator.cs
@@ -0,0 +1,21 @@
+﻿using Core.Components.CellComponent;
+using Core.Components.WorldCellComponent.Cells;
+using UnityEngine;
+using Wooff.MonoIntegration;
+
+namespace Core.Entities.Cells
+{
+    public class CellWorldCreator : MonoEntity
+    {
+        [SerializeField] private int _players;
+        [SerializeField] private WorldCellsConfig _worldCellsConfig;
+
+        private void Start()
+        {
+            var worldCells = (WorldCells)ContextAdd(new WorldCells(_worldCellsConfig, this));
+            worldCells.InitWorld();
+            for (int i = 0; i < _players; i++)
+                worldCells.ChangeRandomCellTo(CellType.City);
+        }
+    }
+}
