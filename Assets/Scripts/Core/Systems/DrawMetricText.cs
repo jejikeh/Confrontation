89a5ad4e7301401d834b1354fc42d05c0a3ed933
jejikeh <jejikeh@gmail.com>
2023-02-24 00:32:38 +0300

    update systems to wotk with new ownership and metric components

--- Assets/Scripts/Core/Systems/DrawMetricText.cs
+++ Assets/Scripts/Core/Systems/DrawMetricText.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Core.Components.MetricBonusComponent;
-using Core.Components.MetricComponent;
+using Core.Components.Metrics.MetricComponent;
+using Core.Components.PlayerComponent;
 using Core.Components.UIComponents.WindowComponent;
 using Core.Components.UIComponents.WindowComponent.Windows;
 using Core.Entities.MetricsKeeper;
 using Core.Entities.UI;
-using UnityEngine;
 using Wooff.ECS.Contexts;
 using Wooff.MonoIntegration;
 
@@ -16,9 +15,12 @@ namespace Core.Systems
     {
         public void Process(float timeScale, IContext<IMonoEntity, List<IMonoEntity>> data)
         {
+            var player = data.Items.Select(x => x.ContextGetAs<Player>()).Where(x => x is not null)
+                .FirstOrDefault(x => x.PlayerType == PlayerType.User);
+            
             (ScreenPlacer.GetWindow(WindowType.Metrics) as MetricsWindow)?.UpdateMetrics(
-                MetricsKeeperManager.GetMetric(MetricType.Gold),
-                MetricsKeeperManager.GetMetric(MetricType.SpeedCreationUnits));
+                player?.MetricHandler.GetMetricByType(MetricType.Gold) as Metric, 
+                player?.MetricHandler.GetMetricByType(MetricType.SpeedCreationUnits) as Metric);
         }
     }
 }
--- Assets/Scripts/Core/Systems/MetricsBonuses.cs
+++ Assets/Scripts/Core/Systems/MetricsBonuses.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Core.Components.MetricBonusComponent.MetricBonusManager;
-using Core.Entities.MetricsKeeper;
+using Core.Components.Metrics.MetricMinerComponent.MetricMinerManager;
+using Core.Components.PlayerComponent;
+using Core.Components.Properties.PropertyComponent;
+using Core.Components.Properties.PropertyOwnerComponent;
 using Wooff.ECS.Contexts;
 using Wooff.MonoIntegration;
 
@@ -9,7 +11,7 @@ namespace Core.Systems
 {
     public class MetricsBonuses : IMonoSystem
     {
-        private const float Time = 10f;
+        private const float Time = 2f;
         private float _timer;
         
         public void Process(float timeScale, IContext<IMonoEntity, List<IMonoEntity>> data)
@@ -20,11 +22,39 @@ namespace Core.Systems
                 return;
             }
 
-            var metricBonus = data.Items.Select(x => x.ContextGet<MetricBonusesHandler>()).Where(x => x is not null);
-            foreach (var bonus in metricBonus.Select(x => x.Items))
-                foreach (var metric in bonus)
-                    MetricsKeeperManager.GetMetric(metric.MetricType).AddToMetric(metric.GetBonusAmount());
+            /*
+            var metricBonuses =
+                data.Items.Select(x => x.ContextGet<MetricMinerHandler>())
+                    .Where(x => x is not null).ToList();
 
+            foreach (var player in players)
+                foreach (var metric in metricBonuses.Where(x => x.Handler.Co).SelectMany(bonus => bonus.Items))
+                    player.MetricHandler.GetMetricByType(metric.MetricType).AddToMetric(metric.GetBonusAmount());
+            */
+            
+            var players = data.Items
+                .Where(x => x.ContextGetAs<Player>() is not null)
+                .ToList();
+
+            foreach (var player in players)
+            {
+                var propertyHandler = player.ContextGet<PropertyHandler>();
+                foreach (var property in propertyHandler.Items.Select(x => x as Property))
+                {
+                    foreach (var metric in player
+                                 .ContextGetAs<Player>()
+                                 .MetricHandler.Items)
+                    {
+                        var metricMiner = property?.Handler.ContextGet<MetricMinerHandler>();
+                        if(metricMiner == null || !metricMiner.ContainsMetric(metric.MetricType))
+                            continue;
+                        
+                        var bonusAmount = metricMiner.GetMetricMiner(metric.MetricType).GetBonusAmount();
+                        metric.AddToMetric(bonusAmount);
+                    }
+                }
+            }
+            
             _timer = 0;
         }
     }
