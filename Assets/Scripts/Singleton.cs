d763f3056f6af159b7f1889862fecfa9c0d0115d
jejikeh <jejikeh@gmail.com>
2023-02-24 00:33:18 +0300

    refactor empty line

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -14,7 +14,7 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
             return _instance;
         }
     }
-
+    
     protected virtual void Awake()
     {
         if (_instance is null)
