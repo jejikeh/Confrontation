110c4f277d90ed04a6b3e68dc3dc6aab80ddcaa4
jejikeh <jejikeh@gmail.com>
2023-02-24 03:44:26 +0300

    player icon presentation

--- Assets/Scripts/Core/Entities/PropertyTagIconVisualisation/PropertyIconConfigManager.cs
+++ Assets/Scripts/Core/Entities/PropertyTagIconVisualisation/PropertyIconConfigManager.cs
@@ -1,7 +1,18 @@
-﻿namespace Core.Entities.PropertyTagIconVisualisation
+﻿using System.Collections.Generic;
+using Core.Components.PlayerComponent;
+using UnityEngine;
+using Wooff.MonoIntegration;
+
+namespace Core.Entities.PropertyTagIconVisualisation
 {
-    public class PropertyIconConfigManager
+    public class PropertyIconConfigManager : MonoEntity
     {
-        
+        [SerializeField] private List<KeyValuePair<GameObject, PlayerType>> _playerIcons =
+            new List<KeyValuePair<GameObject, PlayerType>>();
+
+        private void Start()
+        {
+            
+        }
     }
 }
--- Assets/Scripts/Core/Entities/PropertyTagIconVisualisation/PropertyTagPresentation.cs
+++ Assets/Scripts/Core/Entities/PropertyTagIconVisualisation/PropertyTagPresentation.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using Core.Entities.Camera;
+using Wooff.MonoIntegration;
+
+namespace Core.Entities.PropertyTagIconVisualisation
+{
+    public class PropertyTagPresentation : MonoEntity
+    {
+        private void LateUpdate() {
+            transform.LookAt(transform.position + MonoWorld.GetEntity<SmoothCamera>().transform.forward);
+        }
+    }
+}
