bfaaeadb18e45f188e47b6f24613adf5ba41d319
jejikeh <jejikeh@gmail.com>
2023-02-24 03:42:53 +0300

    new icons prefab

---
+++ Assets/Scripts/Core/Components/PlayerComponent/PlayerConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Components.PlayerComponent
+{
+    public class PlayerConfig
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Core/Entities/PropertyTagIconVisualisation/PropertyIconConfigManager.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Entities.PropertyTagIconVisualisation
+{
+    public class PropertyIconConfigManager
+    {
+        
+    }
+}
