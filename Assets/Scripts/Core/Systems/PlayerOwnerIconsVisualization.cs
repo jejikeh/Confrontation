be1dc9d8d59cf2978f3462800e403e09936002f2
jejikeh <jejikeh@gmail.com>
2023-02-27 03:25:03 +0300

    now cells can override property tag icon

--- Assets/Scripts/Core/Systems/PlayerOwnerIconsVisualization.cs
+++ Assets/Scripts/Core/Systems/PlayerOwnerIconsVisualization.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Core.Components.InformationComponent;
 using Core.Components.PlayerComponent;
 using Core.Components.Properties.PropertyComponent;
 using Core.Components.Properties.PropertyOwnerComponent;
@@ -14,7 +15,7 @@ namespace Core.Systems
 {
     public class PlayerOwnerIconsVisualization : IMonoSystem
     {
-        private List<Property> _properties = new List<Property>();
+        private readonly List<Property> _properties = new List<Property>();
 
         public void Process(float timeScale, IContext<IMonoEntity, List<IMonoEntity>> data)
         {
@@ -32,15 +33,18 @@ namespace Core.Systems
                 {
                     if(_properties.Contains(property))
                         continue;
-                    
+
+                    var informationData = property.ComponentHandler.ContextGet<Information>();
+                    var iconPrefab = informationData is null ? player.Config.InformationConfig.VisualizeIcon : informationData.Config.VisualizeIcon;
                     var propertyTagPresentation = 
                         StaticMonoWorldFinder.SpawnEntity<PropertyTagPresentation>(
-                            player.Config.VisualizeIcon,
+                            iconPrefab,
                             ((Property)property).Handler.MonoObject.transform.position + Vector3.down);
 
+                    // TODO: переместить иконку с игрока на владения. Это позволит менять иконки для разных зданий, а для обозначения игроков оставить цвет
                     propertyTagPresentation.MonoObject.transform.GetChild(0).GetComponent<SpriteRenderer>().color = player.Config.VisualizeColor;
                     propertyTagPresentation.MonoObject.transform.SetParent((property as Property)?.Handler.MonoObject.transform);
-                    propertyTagPresentation.MonoObject.transform.DOLocalMove(Vector3.up, 0.5f);
+                    propertyTagPresentation.MonoObject.transform.DOLocalMove(Vector3.up * 1.3f, 0.5f);
                     _properties.Add(property as Property);
                 }
             }
