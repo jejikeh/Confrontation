814a5974862bbaaceb1284318c7884b28fbc43bf
jejikeh <jejikeh@gmail.com>
2023-02-27 23:48:00 +0300

    computer logic

--- Assets/Scripts/Core/Components/PlayerComponent/Players/Computer.cs
+++ Assets/Scripts/Core/Components/PlayerComponent/Players/Computer.cs
@@ -1,4 +1,12 @@
-﻿using Wooff.MonoIntegration;
+﻿using System.Threading.Tasks;
+using Core.Components.CellComponent;
+using Core.Components.ClickableComponent;
+using Core.Components.ClickComponent;
+using Core.Components.Metrics.MetricComponent;
+using Core.Components.Properties.PropertyOwnerComponent;
+using Core.Entities;
+using Core.Entities.Camera;
+using Wooff.MonoIntegration;
 
 namespace Core.Components.PlayerComponent.Players
 {
@@ -6,6 +14,23 @@ namespace Core.Components.PlayerComponent.Players
     {
         public Computer(PlayerConfig data, IMonoEntity handler) : base(data, handler)
         {
+        }
+
+        public override async Task OnTurn()
+        {
+            await base.OnTurn();
+            var cellWorldCreator = StaticMonoWorldFinder.FindCellWorldCreator();
+            while (MetricHandler.GetMetricByType(MetricType.MovePoints).Amount > 0)
+            {
+                await Task.Delay(250);
+                var randomCell = cellWorldCreator.GetFreeCellForBuy(Handler.ContextGet<PropertyHandler>());
+                BuyCell(randomCell, Cell.RandomBuildingCellType());
+                StaticMonoWorldFinder
+                    .GetEntity<SmoothCamera>()?
+                    .ContextGet<Click>()
+                    .StartClick(randomCell.Handler.ContextGet<Clickable>());
+                await Task.Delay(1000);
+            }
         }
     }
 }
