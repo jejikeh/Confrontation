ab624a09757b7bd1b129152ee511550a47b74ed5
jejikeh <jejikeh@gmail.com>
2023-03-11 13:35:50 +0300

    new property components

--- Assets/Scripts/Core/Components/PropertyComponent.cs
+++ Assets/Scripts/Core/Components/PropertyComponent.cs
@@ -1,7 +1,21 @@
-﻿namespace Core.Components
+﻿using Wooff.ECS.Components;
+using Wooff.ECS.Entities;
+
+namespace Core.Components
 {
-    public class PropertyComponent
+    public class PropertyComponent : IComponent
     {
-        
+        public IEntity Owner { get; private set; }
+
+        public PropertyComponent(IEntity owner)
+        {
+            Owner = owner;
+        }
+
+        public void GiveOwnershipToEntity(IEntity newOwner, IEntity self)
+        {
+            Owner = newOwner;
+        }
+
     }
 }
