0de4db40bda3db350e06cf145c86b307d3f3af64
jejikeh <jejikeh@gmail.com>
2023-03-11 13:32:53 +0300

    new randomable component

---
+++ Assets/Scripts/Core/Components/RandomableComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using Wooff.ECS.Components;
+using Random = UnityEngine.Random;
+
+namespace Core.Components
+{
+    [Serializable]
+    public class RandomableComponent : IComponent
+    {
+        [Range(0, 100)]
+        public int Chance;
+
+        public bool GenerateMe()
+        {
+            return Random.Range(0, 100) <= Chance;
+        }
+    }
+}
