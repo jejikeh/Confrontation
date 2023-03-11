fee028767a7318bf99832c5459a5c9d52637d0ce
jejikeh <jejikeh@gmail.com>
2023-03-11 13:22:34 +0300

    update systems, add Start and mapping

--- Assets/Scripts/Wooff.ECS/Systems/ISystem.cs
+++ Assets/Scripts/Wooff.ECS/Systems/ISystem.cs
@@ -1,12 +1,17 @@
-using System.Collections.Generic;
+using System;
 using Wooff.ECS.Contexts;
 using Wooff.ECS.Entities;
 
 namespace Wooff.ECS.Systems 
 {
-    public interface ISystem<T> : IProcessable<IContext<T, List<T>>>, IContextItem 
-        where T : IContextItem, IEntity<T>
+    public interface ISystem : IProcessable<IContext<IEntity>>
     {
+        public void StartFromEntityContextQuery(EntityContext context);
+        public void StartFromEntityQuery(IContextQueryable<IEntity> context);
+        public void StartFromEntityContext(IContext<IEntity> context);
 
+        public void UpdateFromEntityContextQuery(float timeScale, EntityContext context);
+        public void UpdateFromEntityQuery(float timeScale, IContextQueryable<IEntity> context);
+        public void UpdateFromEntityContext(float timeScale, IContext<IEntity> context);
     }
 }
