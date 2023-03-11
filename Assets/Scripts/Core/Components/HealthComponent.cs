0dd0d77c039a405c96a05aea4fd5d1b609f1d91c
jejikeh <jejikeh@gmail.com>
2023-03-11 13:37:10 +0300

    new heath component to work with health

--- Assets/Scripts/Core/Components/HealthComponent.cs
+++ Assets/Scripts/Core/Components/HealthComponent.cs
@@ -1,7 +1,48 @@
-﻿namespace Core.Components
+﻿using System;
+using UnityEngine;
+using Wooff.ECS.Components;
+
+namespace Core.Components
 {
-    public class HealthComponent
+    [Serializable]
+    public class HealthComponent : IComponent
     {
-        
+        [Header("Health Component")]
+        [SerializeField] 
+        public float MaxHealth;
+        public float Health { get; private set; }
+
+        public HealthComponent(HealthComponent healthComponent)
+        {
+            MaxHealth = healthComponent.MaxHealth;
+            Health = MaxHealth;
+        }
+
+        public HealthComponent(float maxHealth)
+        {
+            Health = maxHealth;
+            MaxHealth = maxHealth;
+        }
+
+        public void Heal(float healAmount)
+        {
+            if (healAmount > 0)
+                Health += healAmount;
+
+            Health = Mathf.Clamp(Health, 0, MaxHealth);
+        }
+
+        public void Damage(float damage)
+        {
+            if (damage > 0)
+                Health -= damage;
+
+            Health = Mathf.Clamp(Health, 0, MaxHealth);
+        }
+
+        public void Kill()
+        {
+            Health = 0;
+        }
     }
 }
