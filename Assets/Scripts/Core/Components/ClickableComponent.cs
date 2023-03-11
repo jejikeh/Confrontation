8805e054f0cd9cd2efc01ea7efd54707be484aa1
jejikeh <jejikeh@gmail.com>
2023-03-11 13:32:31 +0300

    new component to work with click system

--- Assets/Scripts/Core/Components/ClickableComponent.cs
+++ Assets/Scripts/Core/Components/ClickableComponent.cs
@@ -1,7 +1,24 @@
-﻿namespace Core.Components
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+using Wooff.ECS.Components;
+
+namespace Core.Components
 {
-    public class ClickableComponent
+    public class ClickableComponent : IComponent
     {
-        
+        public bool Clicked { get; private set; }
+        [CanBeNull] public EventHandler OnClick;
+
+        public void ClickOnMe()
+        {
+            Clicked = true;
+            OnClick?.Invoke(this,EventArgs.Empty);
+        }
+
+        public void StateIsHandled()
+        {
+            Clicked = false;
+        }
     }
 }
